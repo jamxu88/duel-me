3367dd6bfc1be8497eed632b6ac6c8a490c5d07e
jamxu88 <43918220+jamxu88@users.noreply.github.com>
2018-10-09 17:39:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on("ready", () => {
-  console.log("Admin Alert Online");
+  console.log("Bot Ready");
 });
-const prefix = "!!!";
+const prefix = "=";
 client.on("message", (message) => {
   if (!message.content.startsWith(prefix)) return;
   if (!message.content.startsWith(prefix) || message.author.bot) return;
-  if (message.content.startsWith(prefix + "alert")) {
-    let members = message.channel.members;
-    let jam = members.find('id', '289523788822085632');
-    let gold = members.find('id', '332611268085153792');
-    let spig = members.find('id', '275642801545281536');
-    let celery = members.find('id', '204664162729590793');
-    jam.send('Alert! A user needs you.');
-    gold.send('Alert! A user needs you.');
-    console.log("Admin Alert!");
-    message.channel.send("An admin has been alerted.");	
+  if (message.content.startsWith(prefix + "help")) {
+    message.channel.send("Just say =duelme to start dueling. To make the bot make a move, say =use.");
+  } else
+  if (message.content.startsWith(prefix + "duel")) {
+  	  message.channel.send("-duel " + message.author.toString() + "" );
   }else
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("Pong!");
-    console.log("Pinged");
-	}
+  if (message.content.startsWith(prefix + "use")) {
+  	  message.channel.send("-use 1");
+  }
 });
-client.login("NDk5MDMxNzI1OTU2MTM2OTgw.Dp2XOQ.Ec3P_hg9Ymjv-fNP_dkAiZW-2-g");
+client.login(process.env.BOT_TOKEN);
